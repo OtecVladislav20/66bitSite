d54b187e619d1a23af4ba1c30c93e7531c036742
Vladislav Maslakov <79398635+OtecVladislav20@users.noreply.github.com>
2025-08-31 19:18:42 +0500

    Add list of staff

--- src/pages/staff-list-page.tsx
+++ src/pages/staff-list-page.tsx
@@ -1,12 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/header/header';
+import { Breadcrumbs } from '../components/breadcrumbs/breadcrumbs';
 
 
 export default function StaffList(): JSX.Element {
+  const navigate = useNavigate();
   return (
     <>
       <Header/>
       <main>
-        {/* <Breadcrumbs/> */}
+        <Breadcrumbs/>
 
         <section className='search-wrapper flex'>
           <div className='search__block'>
@@ -44,6 +47,29 @@ export default function StaffList(): JSX.Element {
               </button>
             </div>
             <button className='filter__btn'>Найти</button>
+          </div>
+        </section>
+
+        <section className='staff__list-wrapper flex'>
+          <div className='staff__list'>
+            <table className='staff__table'>
+              <thead>
+                <tr>
+                  <td className='td__title'>ФИО</td>
+                  <td className='td__title'>Должность</td>
+                  <td className='td__title'>Телефон</td>
+                  <td className='td__title'>Дата рождения</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='staff__item' onClick={() => navigate('/staff-list/12312331')}>
+                  <td>Дмитриев Игорь Степанович</td>
+                  <td>Дизайнер</td>
+                  <td>+7 934 349-43-23</td>
+                  <td>23.09.2000</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </section>
       </main>
